8d6ec15860f5677e2c6e7233439034a4e6b296b3
Omar Sebastian Rodriguez Reyes <sebastianomarrodriguezreyes@gmail.com>
2026-04-21 13:36:01 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double num1, num2, resultado = 0;
+        string operacion;
+
+        Console.WriteLine("=== CALCULADORA ===");
+
+        Console.Write("Digite el primer número: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite el segundo número: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite la operación (+, -, *, /): ");
+        operacion = Console.ReadLine();
+
+        if (operacion == "+")
+        {
+            resultado = num1 + num2;
+            Console.WriteLine("Resultado: " + resultado);
+        }
+        else if (operacion == "-")
+        {
+            resultado = num1 - num2;
+            Console.WriteLine("Resultado: " + resultado);
+        }
+        else if (operacion == "*")
+        {
+            resultado = num1 * num2;
+            Console.WriteLine("Resultado: " + resultado);
+        }
+        else if (operacion == "/")
+        {
+            if (num2 != 0)
+            {
+                resultado = num1 / num2;
+                Console.WriteLine("Resultado: " + resultado);
+            }
+            else
+            {
+                Console.WriteLine("Error: no se puede dividir entre cero.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Operación no válida.");
+        }
+
+        Console.WriteLine("Presione una tecla para salir...");
+        Console.ReadKey();
+    }
+}
